36b732e66e9a8a8ce976ec95442baa5695984756
xuejin13 <xuejingao@gmail.com>
2020-01-29 13:41:01 -0500

    Problem with displaying the tableview

--- Passion/Passion/PersonalViewController.swift
+++ Passion/Passion/PersonalViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 import Parse
 
-class PersonalViewController: UIViewController {
+import AlamofireImage
+import Alamofire
+
+class PersonalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var nameField: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         let user = PFUser.current()
         nameField.text = user?.username
+        
     }
     
     @IBAction func onLogout(_ sender: Any) {
@@ -31,7 +41,52 @@ class PersonalViewController: UIViewController {
         delegate.window?.rootViewController = loginViewController
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "PersonalPost")
+        
+//        query.whereKey("author", equalTo: PFUser.current()!)
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+        
+//        print(query)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonalPostCell") as! PersonalCellTableViewCell
+        
+        let post = posts[indexPath.row]
+        
+        let date = post["date"] as! String
+        cell.dateLabel.text = date
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        let caption = post["caption"] as! String
+        cell.captionLabel.text = caption
+        
+//        print(caption)
+//        print(url)
+        
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
