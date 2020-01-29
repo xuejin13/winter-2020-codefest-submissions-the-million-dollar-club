c30a8a462e1d0102684cce0daabb79e3a392f139
xuejin13 <xuejingao@gmail.com>
2020-01-29 15:17:49 -0500

    Still working on the exercise display/backend

---
+++ Passion/Passion/SelectExerciseViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SelectExerciseViewController.swift
+//  Passion
+//
+//  Created by Xuejin Gao on 1/29/20.
+//  Copyright © 2020 Xuejin Gao. All rights reserved.
+//
+
+import UIKit
+
+class SelectExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onDone(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
